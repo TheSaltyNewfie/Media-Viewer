b58da0a191f561ff215c954e9c29ef1b5159e473
Toby Dodge <46291294+TheSaltyNewfie@users.noreply.github.com>
2022-11-09 09:32:18 -0330

    trying to add debug stuff and ways to determine media

--- Utils.cs
+++ Utils.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MediaViewer
 {
     class Utils
     {
+        public string determineMedia(string File)
+        {
+            string temp
+            
+            for(int i = 0; i < File.length(); ++i)
+            {
 
+            }
+        }
+    }
+
+    class Debug
+    {
+        StreamWriter writer = new StreamWriter("./logs/")
+
+        public static int log(string details)
+        {
+            writer.WriteLine(details);
+        }
     }
 }
